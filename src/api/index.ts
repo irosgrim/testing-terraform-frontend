0bdd35540a3bdad3f0e87a839f5857d412c62e4a
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-10-18 22:04:44 +0200

    use wss

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 export let BACKEND_BASE_URL = "https://ioan-backend-service-jucwb6gsyq-ez.a.run.app";
-export let WS_BACKEND_URL = "ws://ioan-backend-service-jucwb6gsyq-ez.a.run.app"
+export let WS_BACKEND_URL = "wss://ioan-backend-service-jucwb6gsyq-ez.a.run.app"
 
 const urlApiKey = new URLSearchParams(window.location.search).get("api_key");
 const API_KEY = urlApiKey ? `?api_key=${urlApiKey}` : "";
